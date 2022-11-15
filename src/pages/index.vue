f11591d63d1356218dd0f748145493430acea698
Adrian Shajkofci <adrian.shajkofci@bnovate.com>
2022-11-15 17:37:56 +0100

    Update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -40,13 +40,79 @@ import CardStatisticsVertical from '@core/components/CardStatisticsVertical.vue'
           md="12"
         >
           <VCard
-            title="Parameters"
+            title="Live view parameters"
             class="position-relative"
           >
             <VCardText>
-              <VBtn size="small">
-                Start
-              </VBtn>
+              <VForm>
+                <VRow>
+                  <VCol
+                    md="6"
+                    cols="12"
+                  >
+                    <VTextField
+                      label="Event buffer"
+                      suffix="[s]"
+                    />
+                  </VCol>
+
+                  <VCol
+                    md="6"
+                    cols="12"
+                  >
+                    <VSelect
+                      label="Trigger channel"
+                      :items="['FL1', 'FL2', 'SSC', 'FSC']"
+                    />
+                  </VCol>
+                </VRow>
+                <VRow>
+                  <VCol
+                    cols="12"
+                    md="6"
+                  >
+                    <VSelect
+                      label="X-axis"
+                      :items="['FL1', 'FL2', 'SSC', 'FSC']"
+                    />
+                  </VCol>
+                  <VCol
+                    cols="12"
+                    md="6"
+                  >
+                    <VSelect
+                      label="Y-axis"
+                      :items="['FL1', 'FL2', 'SSC', 'FSC']"
+                    />
+                  </VCol>
+                </VRow>
+              </VForm>
+            </VCardText>
+          </VCard>
+        </VCol>
+        <VCol
+          cols="12"
+          md="12"
+        >
+          <VCard
+            title="Laser parameters"
+            class="position-relative"
+          >
+            <VCardText>
+              <VRow>
+                <VCol>
+                  <VRadioGroup inline>
+                    <VRadio
+                      label="Nanobeads"
+                      value="beads"
+                    />
+                    <VRadio
+                      label="3um beads"
+                      value="3um"
+                    />
+                  </VRadioGroup>
+                </VCol>
+              </VRow>
             </VCardText>
           </VCard>
         </VCol>
@@ -59,9 +125,112 @@ import CardStatisticsVertical from '@core/components/CardStatisticsVertical.vue'
             class="position-relative"
           >
             <VCardText>
-              <VBtn size="small">
-                Start
-              </VBtn>
+              <VRow>
+                <VCol
+                  md="12"
+                  cols="12"
+                >
+                  <VTextField
+                    label="Integration time"
+                    suffix="[s]"
+                  />
+                </VCol>
+              </VRow>
+              <VRow>
+                <VCol>
+                  <VTable density="compact">
+                    <thead>
+                      <tr>
+                        <th class="text-uppercase">
+                          Channel
+                        </th>
+                        <th class="text-center text-uppercase">
+                          Median
+                        </th>
+                        <th class="text-center text-uppercase">
+                          rCV [%]
+                        </th>
+                        <th class="text-center text-uppercase">
+                          Offset
+                        </th>
+                        <th class="text-center text-uppercase">
+                          Valid
+                        </th>                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          SSC
+                        </td>
+                        <td class="text-center">
+                          110
+                        </td>
+                        <td class="text-center">
+                          45
+                        </td>
+                        <td class="text-center">
+                          -55.
+                        </td>
+                        <td class="text-center">
+                          🐸
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          FL1
+                        </td>
+                        <td class="text-center">
+                          110
+                        </td>
+                        <td class="text-center">
+                          45
+                        </td>
+                        <td class="text-center">
+                          -55.
+                        </td>
+                        <td class="text-center">
+                          🐙
+                        </td>
+                      </tr>    
+                      <tr>
+                        <td>
+                          FL2
+                        </td>
+                        <td class="text-center">
+                          110
+                        </td>
+                        <td class="text-center">
+                          45
+                        </td>
+                        <td class="text-center">
+                          -55.
+                        </td>
+                        <td class="text-center">
+                          🐸
+                        </td>
+                      </tr>    
+                      <tr>
+                        <td>
+                          FSC
+                        </td>
+                        <td class="text-center">
+                          110
+                        </td>
+                        <td class="text-center">
+                          45
+                        </td>
+                        <td class="text-center">
+                          -55.
+                        </td>
+                        <td class="text-center">
+                          🐸
+                        </td>
+                      </tr>                                  
+                    </tbody>
+                  </VTable>
+                </VCol>
+              </VRow>
             </VCardText>
           </VCard>
         </VCol>
@@ -80,6 +249,21 @@ import CardStatisticsVertical from '@core/components/CardStatisticsVertical.vue'
             </VCardText>
           </VCard>
         </VCol>
+        <VCol
+          cols="12"
+          md="12"
+        >
+          <VCard
+            title="Save"
+            class="position-relative"
+          >
+            <VCardText>
+              <VBtn size="small">
+                Start
+              </VBtn>
+            </VCardText>
+          </VCard>
+        </VCol>        
       </VRow>
     </VCol>
   </VRow>
